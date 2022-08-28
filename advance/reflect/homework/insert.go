fb6c84f5d7aff9b29b62010363073b691a7ca42c
liqxliqx123 <liqxliqx@126.com>
2022-08-28 21:34:53 +0800

    add reflect insert

--- advance/reflect/homework/insert.go
+++ advance/reflect/homework/insert.go
@@ -1,35 +1,87 @@
 package homework
 
 import (
+	"database/sql/driver"
 	"errors"
+	"reflect"
+	"strings"
 )
 
 var errInvalidEntity = errors.New("invalid entity")
 
-// InsertStmt 作业里面我们这个只是生成 SQL，所以在处理 sql.NullString 之类的接口
-// 只需要判断有没有实现 driver.Valuer 就可以了
+func InsertStmtCore(entity interface{}) (string, []string, []interface{}, error) {
+	var cols []string
+	var args []interface{}
+	var tableName string
+
+	typ := reflect.TypeOf(entity)
+
+	if entity == nil {
+		return "", nil, nil, errInvalidEntity
+	}
+
+	val := reflect.ValueOf(entity)
+
+	if val.Kind() == reflect.Ptr {
+		for val.Elem().Kind() == reflect.Ptr {
+			val = val.Elem()
+		}
+		if val.Elem().Kind() == reflect.Struct {
+			val = val.Elem()
+		}
+	}
+
+	typ = val.Type()
+	if typ.Kind() == reflect.Struct && typ.NumField() == 0 {
+		return "", nil, nil, errInvalidEntity
+	}
+	tableName = typ.Name()
+
+	for i := 0; i < typ.NumField(); i++ {
+		fd := typ.Field(i)
+		fv := val.Field(i)
+
+		if fd.Type.Kind() == reflect.Struct && !(fd.Type.Implements(reflect.TypeOf((*driver.Valuer)(nil)).Elem())) {
+			//fmt.Println(fd.Type.Implements(reflect.TypeOf((*sql.Scanner)(nil)).Elem()))
+			_, cols2, args2, err := InsertStmtCore(fv.Interface())
+			if err != nil {
+				return "", nil, nil, err
+			}
+			cols = append(cols, cols2...)
+			args = append(args, args2...)
+		} else {
+			cols = append(cols, "`"+fd.Name+"`")
+			args = append(args, fv.Interface())
+		}
+	}
+
+	return tableName, cols, args, nil
+}
+
 func InsertStmt(entity interface{}) (string, []interface{}, error) {
 
-	// val := reflect.ValueOf(entity)
-	// typ := val.Type()
-	// 检测 entity 是否符合我们的要求
-	// 我们只支持有限的几种输入
+	tableName, cols, args, err := InsertStmtCore(entity)
+	if err != nil {
+		return "", nil, err
+	}
 
-	// 使用 strings.Builder 来拼接 字符串
-	// bd := strings.Builder{}
+	bd := strings.Builder{}
+	bd.WriteString("INSERT INTO `")
+	bd.WriteString(tableName)
+	bd.WriteString("`")
 
-	// 构造 INSERT INTO XXX，XXX 是你的表名，这里我们直接用结构体名字
+	//sqlStr := "INSERT INTO `" + tableName + "`"
+	//fmt.Println(reflect.TypeOf(entity).Name())
+	placeholder := make([]string, len(args))
+	for p := 0; p < len(placeholder); p++ {
+		placeholder[p] = "?"
+	}
+	//sqlStr += "(" + strings.Join(cols, ",") + ") VALUES(" + strings.Join(placeholder, ",") + ");"
+	bd.WriteString("(")
+	bd.WriteString(strings.Join(cols, ","))
+	bd.WriteString(") VALUES(")
+	bd.WriteString(strings.Join(placeholder, ","))
+	bd.WriteString(");")
 
-	// 遍历所有的字段，构造出来的是 INSERT INTO XXX(col1, col2, col3)
-	// 在这个遍历的过程中，你就可以把参数构造出来
-	// 如果你打算支持组合，那么这里你要深入解析每一个组合的结构体
-	// 并且层层深入进去
-
-	// 拼接 VALUES，达成 INSERT INTO XXX(col1, col2, col3) VALUES
-
-	// 再一次遍历所有的字段，要拼接成 INSERT INTO XXX(col1, col2, col3) VALUES(?,?,?)
-	// 注意，在第一次遍历的时候我们就已经拿到了参数的值，所以这里就是简单拼接 ?,?,?
-
-	// return bd.String(), args, nil
-	panic("implement me")
+	return bd.String(), args, nil
 }
